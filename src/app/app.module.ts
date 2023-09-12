30a355919314b0d5730e9578b7a54f53a33a1bda
majohorta0429 <majohorta0429@gmail.com>
2023-09-11 21:56:43 -0500

    Se agrega el proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
+//Modulos
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+//Componentes
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GastosComponent } from './components/gastos/gastos.component';
+import { IngresarPresupuestoComponent } from './components/ingresar-presupuesto/ingresar-presupuesto.component';
+import { IngresarGastoComponent } from './components/gastos/ingresar-gasto/ingresar-gasto.component';
+import { ListarGastoComponent } from './components/gastos/listar-gasto/listar-gasto.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GastosComponent,
+    IngresarPresupuestoComponent,
+    IngresarGastoComponent,
+    ListarGastoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
